3905564b418aef4bb1e0d17b31fd4c993a41543e
Roshan Chouhan <100076376+roshanchouha@users.noreply.github.com>
2023-05-25 02:34:19 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,19 +1,48 @@
 <?php include 'header.php'; ?>
+
       <section>
          <div class="logcontainer">
             <div class="loginfo">
                 <h2>Groco<i class="fa fa-shopping-basket" aria-hidden="true"></i></h2>
-
             </div>
             <div class="logpage">
-                  <form class="login-form" action="index.php" method="post" >
+                  <form class="login-form" action="<?php echo   $_SERVER['PHP_SELF'] ?>" method="post" >
                     <h1>Login Now</h1>
-                     <input class="box" type="email" name="" value="" placeholder="your email">
-                     <input class="box" type="password" name="" value="" placeholder="password">
+                     <input class="box" type="email" name="email" value="" placeholder="your email">
+                     <input class="box" type="password" name="password" value="" placeholder="password">
                      <p> Forget Your password  <a href="#" >Click Here</a></p>
                      <p> Don't Have An Account  <a href="Create.php" >Create Now</a></p>
-                     <input class="btn" type="submit" name="" value="Login Now" >
+                     <input class="btn" type="submit" name="login" value="Login Now" >
                   </form>
+                  <?php
+
+                       if(isset($_POST['login']))
+                      {
+                         $username=$_POST['email'];
+                         $password=md5($_POST['password']) ;
+                         include 'config.php';
+                       $sql="SELECT UID,emailid,password FROM users WHERE emailid = '{$username}'  AND  password='{$password}'";
+                         $result=mysqli_query($conn,$sql);
+
+                             if( mysqli_num_rows($result)>0)
+                             {
+                              while ($row=mysqli_fetch_assoc($result))
+                               {
+                                    session_start();
+                                     $_SESSION['email']= $row['emailid'];
+                                     $_SESSION['UID']= $row['UID'];
+                                   header("Location: http://localhost/grocery%20store/index.php");
+
+                                }
+                              }
+
+                             else{
+                              echo  '<p class="alert alert-danger"> User not exsits</p>';
+                             }
+
+                          mysqli_close($conn);
+                         }
+                   ?>
             </div>
          </div>
       </section>
